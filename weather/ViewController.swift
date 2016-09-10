b15692e0412c8107ddc7b542c3155fe61ae31628
gaberdios <gaberdios@gmail.com>
2016-09-10 19:21:46 +0200

    Done

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -7,17 +7,44 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
+import SwiftyJSON
+
 
 class ViewController: UIViewController {
+    @IBOutlet var marsPhotoImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let dateString = getDateString()
+        Alamofire.request(.GET, "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos", parameters: ["api_key": "DEMO_KEY", "earth_date": dateString])
+            .responseJSON { response in
+                if let result = response.result.value {
+                    let json = JSON(result)
+                    if let imageURL = json["photos"][0]["img_src"].string {
+                        // Replace "http" with "https" in the image URL.
+                        let httpsURL = imageURL.stringByReplacingOccurrencesOfString("http", withString: "https")
+                        let URL = NSURL(string: httpsURL)!
+                        
+                        // Set the ImageView with an image from a URL
+                        self.marsPhotoImageView.af_setImageWithURL(URL)
+                    }
+                }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func getDateString() -> String {
+        let calendar = NSCalendar.currentCalendar()
+        let yesterday = calendar.dateByAddingUnit(.Day, value: -5, toDate: NSDate(), options: [])
+        let components = calendar.components([.Day , .Month , .Year], fromDate: yesterday!)
+        
+        return "\(components.year)-\(components.month)-\(components.day)"
     }
 
 
